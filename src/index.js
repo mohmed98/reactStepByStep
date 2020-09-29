c02e1f2dba62b4043c121ffe980fea63ccbe1f95
unknown <mohaemdatta447@gmail.com>
2020-09-29 16:56:39 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-const name = 'Josh Perez';
-const element = <h1>Hello, {name}</h1>;
+
+
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>
+    }
+    return <h1>Hello, stranger.</h1>
+}
+const user = {
+    firstName: 'Abbas',
+    lastName: 'Dawo'
+}
+const element = <h1>Hello, {formatName(user)}</h1>;
 ReactDOM.render(
     element,
     document.getElementById('root')
