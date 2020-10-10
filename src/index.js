daacacb52496a91504fb8ff4b521a1a08638e8a9
Your Name Comes HereMOhamed <mohamedatta447@gmail.com>
2020-10-10 13:25:03 +0200

    finished section 5

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from "@testing-library/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState({ date: new Date() });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
+}
 
-
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-}
-function getGreeting(user) {
-    if (user) {
-        return <h1>Hello, {formatName(user)}!</h1>
-    }
-    return <h1>Hello, stranger.</h1>
-}
-const user = {
-    firstName: 'Abbas',
-    lastName: 'Dawo'
-}
-const element = <h1>Hello, {formatName(user)}</h1>;
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
+ReactDOM.render(<Clock />, document.getElementById("root"));
